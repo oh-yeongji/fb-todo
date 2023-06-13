089f4c7eeaeabf6ebef6a5a0f9f37deaab1a4fb5
dev.cappp <dev.cappp@gmail.com>
2023-06-13 15:40:10 +0900

    feat: 렌더링 최적화 진행중

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import List from "./components/List";
 import Form from "./components/Form";
 
 function App() {
+  console.log("App 랜더링");
   // 더미 데이터 state변수
   //useState안에 초기값이 들어가면
   //더미데이터 일반변수에서 state변수가됨
@@ -13,8 +14,6 @@ function App() {
     { id: 3, title: "할일 3", completed: true },
     { id: 4, title: "할일 4", completed: true },
   ]);
-
-
 
   //이벤트 핸들러
 
--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,7 @@ import React from "react";
 import ListItem from "./ListItem";
 
 const List = ({ todoData, setTodoData }) => {
+  console.log("List 랜더링");
   return (
     <div>
       {/* 밑은 자스 자리 */}
@@ -18,4 +19,4 @@ const List = ({ todoData, setTodoData }) => {
   );
 };
 
-export default List;
+export default React.memo(List);
