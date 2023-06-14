8bd93ca601b4e8ae51ed3c97d9bf2d6798d5550d
dev.cappp <dev.cappp@gmail.com>
2023-06-14 12:49:49 +0900

    feat: Todo Title 수정/취소 기능 추가

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListItem = ({ item, todoData, setTodoData }) => {
-  console.log("ListItem 랜더링", item);
-  const btnStyle = {
-    color: "#fff",
-    float: "right",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-  };
+  // console.log("ListItem 랜더링", item);
+  const [isEdit, setIsEdit] = useState(false);
+  //편집 상태 타이틀 설정 state
+  const [editTitle, setEditTitle] = useState(item.title);
+
   const getStyle = _completed => {
     return {
       padding: "10px",
@@ -17,7 +13,29 @@ const ListItem = ({ item, todoData, setTodoData }) => {
     };
   };
   //이벤트 핸들러
-  const handleClick = _id => {
+  const handleEditClick = () => {
+    setIsEdit(true);
+  };
+
+  const handleEditChange = e => {
+    setEditTitle(e.target.value);
+  };
+
+  const handleCancleClick = () => {
+    setIsEdit(false);
+  };
+
+  const handleSaveClick = _id => {
+    let newTodoData = todoData.map(item => {
+      if (item.id === _id) {
+        item.title = editTitle;
+      }
+      return item;
+    });
+    setTodoData(newTodoData);
+    setIsEdit(false); //편집이 끝남
+  };
+  const handleDeleteClick = _id => {
     //전달된 ID를 검색해서 목록에서 제거
     //1. 전달된 ID로 해당하는 목록 찾아서 제외
     //2. 새로운 목록으로 갱신해서 화면 리랜더링
@@ -41,25 +59,60 @@ const ListItem = ({ item, todoData, setTodoData }) => {
     setTodoData(newTodoData);
   };
 
-  return (
-    <div className="flex items-center justify-between w-full mb-2 px-4 py-1 text-gray-600 bg-gray-100 border rounded">
-      <div className="items-center" style={getStyle(item.completed)}>
-        {/* defaultChecked: checkbox에 기본체크 상태 설정 */}
-        <input
-          type="checkbox"
-          defaultChecked={item.completed}
-          onChange={() => handleCompleteChange(item.id)}
-        />
-        {/* html태그에서 자스 출력할때 */}
-        {item.title}
+  if (isEdit) {
+    //편집중
+    return (
+      <div className="flex items-center justify-between w-full mb-2 px-4 py-1 text-gray-600 bg-gray-100 border rounded">
+        <div className="items-center w-3/5">
+          <input
+            className="w-full px-3 py-2 mr-3 text-gray-500 rounded"
+            type="text"
+            value={editTitle}
+            onChange={handleEditChange}
+          />
+        </div>
+        <div className="items-center">
+          <button className="px-4 py-2 float-right" onClick={handleCancleClick}>
+            Cancle
+          </button>
+
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => handleSaveClick(item.id)}
+          >
+            Save
+          </button>
+        </div>
       </div>
-      <div className="items-center">
-        <button style={btnStyle} onClick={() => handleClick(item.id)}>
-          X
-        </button>
+    );
+  } else {
+    //일반 상태
+    return (
+      <div className="flex items-center justify-between w-full mb-2 px-4 py-1 text-gray-600 bg-gray-100 border rounded">
+        <div className="items-center flex" style={getStyle(item.completed)}>
+          {/* defaultChecked: checkbox에 기본체크 상태 설정 */}
+          <input
+            type="checkbox"
+            defaultChecked={item.completed}
+            onChange={() => handleCompleteChange(item.id)}
+          />
+          <span className="ml-3">{item.title}</span>
+        </div>
+        <div className="items-center">
+          <button
+            className="px-4 py-2 float-right"
+            onClick={() => handleDeleteClick(item.id)}
+          >
+            Delete
+          </button>
+
+          <button className="px-4 py-2 float-right" onClick={handleEditClick}>
+            Edit
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 //리랜더링 최적화 적용
